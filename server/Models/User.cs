63f1f1238b3abb6c0e359f479d2e0540f3ec8812
fahim2204 <fahimfaisal1998@gmail.com>
2022-08-30 10:30:09 +0600

    Start Project

---
+++ server/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChatiFy.Models
+{
+    public class User
+    {
+        public int userId { get; set; }
+        public string fName { get; set; }
+        public string lName { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+        public string token { get; set; }
+        public string status { get; set; }
+
+    }
+}
